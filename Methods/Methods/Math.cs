11447c9c74dde93f24a096832a672d46be20e83b
Your Name <you@example.com>
2019-06-05 08:37:14 -0700

    Initial commit for methods console app

---
+++ Methods/Methods/Math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Math
+    {
+        //method that adds 2 to user input
+        public static int addTwo(int num1)
+        {
+            int total;
+            total = 2 + num1;
+            return total;
+
+        }
+
+        //method that multiplies user input by 3
+        public static int timesThree(int num2)
+        {
+            int total;
+            total = 3 * num2;
+            return total;
+        }
+
+        //method that subtracts 5 from user input
+        public static int subtractFive(int num3)
+        {
+            int total;
+            total = num3 - 5;
+            return total;
+        }
+
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now performing some math...");
+            Console.ReadLine();
+
+            // Calls methods from Math class
+            Console.WriteLine(input + " plus 2 equals {0}.", Math.addTwo(input));
+            Console.WriteLine(input + " times 3 equals {0}.", Math.timesThree(input));
+            Console.WriteLine(input + " minus 5 equals {0}.", Math.subtractFive(input));
+            Console.ReadLine();
+
+
+        }
+    }
+}
