b9c5770b5b921d676d53497991b5b06749f32032
Your Name <you@example.com>
2019-06-06 17:28:44 -0700

    Initial commit for VoidMethod

---
+++ VoidMethod/VoidMethod/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Math
+    {
+        public void twoIntegers(int num1, int num2)
+        {
+            int square = num1 * num1;
+            Console.WriteLine(num1 + " times " + num1 + " equals " + square + ".\n" + num2);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call method with two parameters
+            Math math1 = new Math();
+            math1.twoIntegers(3, 8);
+            Console.ReadLine();
+
+            //specify parameters by name
+            Math math2 = new Math();
+            math2.twoIntegers(num1: 4, num2: 7);
+            Console.ReadLine();
+        }
+    }
+}
