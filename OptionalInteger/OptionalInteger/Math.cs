40e277d979250eb17c5f0f6d3723760c46f42b65
Your Name <you@example.com>
2019-06-06 17:36:32 -0700

    Initial commit for OptionalInteger

---
+++ OptionalInteger/OptionalInteger/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalInteger
+{
+    class Math
+    {
+        public static int multiply(int num1, int num2 = 11)
+        {
+            int total = num1 * num2;
+            return total;
+        }
+    }
+}
---
+++ OptionalInteger/OptionalInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number. You can choose to skip this step.");
+            int num2;
+
+            //checks to see if the user enters an integer
+            bool success = Int32.TryParse(Console.ReadLine(), out num2);
+            if (success)
+            {
+                int answer = Math.multiply(num1, num2);
+                Console.WriteLine(num1 + " times " + num2 + " equals " + answer + ".");
+                Console.ReadLine();
+            }
+            else
+            {
+                int answer = Math.multiply(num1);
+                Console.WriteLine(num1 + " times 11 is " + answer + ".");
+                Console.ReadLine();
+            }
+        }
+    }
+}
