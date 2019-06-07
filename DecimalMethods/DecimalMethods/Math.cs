e752414fabe6d6cf5d0b3aba40ac75430e30f370
Your Name <you@example.com>
2019-06-06 17:31:44 -0700

    Initial commit for DecimalMethods

---
+++ DecimalMethods/DecimalMethods/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalMethods
+{
+    class Math
+    {
+        //add 5 to integer input
+        public static int sum(int num1)
+        {
+            int total = num1 + 5;
+            return total;
+        }
+
+        //add 10 to decimal input and rounds to nearest integer
+        public static int sum(decimal num1)
+        {
+            int answer = Convert.ToInt32(num1) + 10;
+            return answer;
+        }
+
+        //convert string to integer and add 8
+        public static int sum(string num1)
+        {
+            int answer = Convert.ToInt32(num1) + 8;
+            return answer;
+        }
+
+    }
+}
---
+++ DecimalMethods/DecimalMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please type a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input + " plus 5 equals {0}.", Math.sum(input));
+            Console.ReadLine();
+
+            Console.WriteLine("Please type a decimal.");
+            decimal input2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(input2 + " plus 10 equals {0}, rounded to the nearest integer.", Math.sum(input2));
+            Console.ReadLine();
+
+            Console.WriteLine("Please type another number.");
+            string input3 = Console.ReadLine();
+            Console.WriteLine(input3 + " plus 8 equals {0}.", Math.sum(input3));
+            Console.ReadLine();
+        }
+    }
+}
