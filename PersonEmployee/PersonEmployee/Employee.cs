6a5f3c6eeaecbbbd2d4757f6a04db74890fd1f05
Your Name <you@example.com>
2019-06-08 15:39:42 -0700

    Change person class to abstract

--- PersonEmployee/PersonEmployee/Employee.cs
+++ PersonEmployee/PersonEmployee/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PersonEmployee
 {
-    class Employee : Person
+    public class Employee : Person
     {
         public int Id { get; set; }
     }
--- PersonEmployee/PersonEmployee/Person.cs
+++ PersonEmployee/PersonEmployee/Person.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace PersonEmployee
 {
-    class Person
+    //abstract class to ensure no instance of this class can be created
+    public abstract class Person
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
