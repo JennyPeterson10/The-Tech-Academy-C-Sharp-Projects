01a7e820911ee2267ed7baf27bd8690fab9d9afb
Your Name <you@example.com>
2019-06-10 11:39:45 -0700

    Initial commit for NumberAmount

---
+++ NumberAmount/NumberAmount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAmount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 4.32M};
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
