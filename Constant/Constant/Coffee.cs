0361a810aded62db19dd3b7154fb1d7031bc09da
Your Name <you@example.com>
2019-06-12 10:48:38 -0700

    Initial commit for Constant

---
+++ Constant/Constant/Coffee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    public class Coffee
+    {
+        // Constructor call chain with a default coffee size 12
+        public Coffee(string name) : this(12, name)
+        {
+        }
+        public Coffee(int amount, string type)
+        {
+            Amount = amount;
+            Type = type;
+        }
+        public int Amount { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Constant/Constant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string coffee = "I always start the day with coffee.";
+            // It is possible to create a new instance of Coffee using var
+            var drink = new Coffee("latte");
+            Console.WriteLine(coffee + " My morning coffe today is a {0} oz {1}.", drink.Amount, drink.Type);
+            Console.ReadLine();
+        }
+    }
+}
