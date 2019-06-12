80d9385a734e7be61340c45750ca5d0f790dc61b
Your Name <you@example.com>
2019-06-12 07:53:31 -0700

    Initial commit for Today

---
+++ Today/Today/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Today
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Current date and time: " + today);
+            Console.WriteLine("Please select a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            // converts the number to a timespan
+            TimeSpan hoursFromNow = new TimeSpan(number, 0, 0);
+            // calculates the time in that many hours from now
+            DateTime future = today.Add(hoursFromNow);
+            Console.WriteLine("In {0} hours from now it will be: " + future, number);
+            Console.ReadLine();
+        }
+    }
+}
