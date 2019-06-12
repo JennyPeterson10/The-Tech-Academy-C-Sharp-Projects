748c5521a91e78cdb28f8ac241c20a90a432afa5
Your Name <you@example.com>
2019-06-12 07:31:35 -0700

    Initial commit for TextFile

---
+++ TextFile/TextFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Please select a number.");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"..\..\log.txt", number);
+            Console.WriteLine("The number you selected is: " + File.ReadAllText(@"..\..\log.txt") + ".");
+            Console.ReadLine();
+        }
+    }
+}
